53fc97ba0d13552d998ecdc22f726a46c38c3b4c
Ozzy Scuba <ozzysp@icloud.com>
2023-11-15 02:45:37 -0300

    Initial setup

---
+++ src/js/engine.js
@@ -0,0 +1,37 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+
+    },
+    values: {
+        timerId: null,
+    },
+}; 
+
+function randomSquare() {
+    state.view.squares.forEach((square=>{
+        square.classList.remove("enemy");
+    }));
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, 500);
+
+}
+
+// function addListnerHitBox () {
+//     state.view.squares.forEach((square) => {
+//         if(square.id === 1)
+// })
+
+function init() {
+    randomSquare();
+}
+
+init();
